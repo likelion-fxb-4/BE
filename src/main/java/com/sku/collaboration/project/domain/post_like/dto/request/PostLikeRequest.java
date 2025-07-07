b15808df825ee6846ef5b84dfe93a0a8d4d8392a
jaeyeon <kasto6329@gmail.com>
2025-07-07 17:54:19 +0900

    ✨ Feature: 게시글 좋아요 기능 구현

--- src/main/java/com/sku/collaboration/project/domain/post_like/dto/request/PostLikeRequest.java
+++ src/main/java/com/sku/collaboration/project/domain/post_like/dto/request/PostLikeRequest.java
@@ -3,17 +3,18 @@ package com.sku.collaboration.project.domain.post_like.dto.request;
 
 import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Getter
 @NoArgsConstructor
-@Schema(title = "PostLikeRequest DTO", description = "게시글 w데이터 전송")
+@Schema(title = "PostLikeRequest DTO", description = "게시글 좋아요 요청")
 
 public class PostLikeRequest {
 
-  @NotBlank(message = "게시글 제목 항목은 필수입니다.")
-  @Schema(description = "게시글 제목", example = "제목입니다.")
-  private String title;
+  @NotNull(message = "좋아요 여부는 필수입니다.")
+  @Schema(description = "좋아요 여부", example = "true")
+  private Boolean like;
 }
   
--- src/main/java/com/sku/collaboration/project/domain/post_like/repository/PostLikeRepository.java
+++ src/main/java/com/sku/collaboration/project/domain/post_like/repository/PostLikeRepository.java
@@ -1,8 +1,13 @@
 package com.sku.collaboration.project.domain.post_like.repository;
 
+import com.sku.collaboration.project.domain.post.entity.Post;
 import com.sku.collaboration.project.domain.post_like.entity.PostLike;
+import com.sku.collaboration.project.domain.user.entity.User;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface PostLikeRepository extends JpaRepository<PostLike, Long> {
-
+  boolean existsByPostIdAndUserId(Long postId, Long userId);
+  int countByPostAndIsDeleteFalse(Post post);
+  Optional<PostLike> findByPostAndUser(Post post, User user);
 }
