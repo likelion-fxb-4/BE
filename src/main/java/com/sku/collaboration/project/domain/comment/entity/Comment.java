65d35370fff19635783655331182a5c09dda91ed
jaeyeon <kasto6329@gmail.com>
2025-07-07 19:35:22 +0900

    ✨ Feature: CASCADE 추가

--- src/main/java/com/sku/collaboration/project/domain/comment/entity/Comment.java
+++ src/main/java/com/sku/collaboration/project/domain/comment/entity/Comment.java
@@ -1,8 +1,11 @@
 package com.sku.collaboration.project.domain.comment.entity;
 
+import com.sku.collaboration.project.domain.comment_likes.entity.CommentLike;
 import com.sku.collaboration.project.domain.post.entity.Post;
 import com.sku.collaboration.project.domain.user.entity.User;
 import jakarta.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
 import lombok.*;
 
 @Entity
@@ -31,4 +34,7 @@ public class Comment {
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "user_id", nullable = false)
     private User user;
+
+    @OneToMany(mappedBy = "comment", cascade = CascadeType.REMOVE, orphanRemoval = true)
+    private List<CommentLike> likes = new ArrayList<>();
 }
--- src/main/java/com/sku/collaboration/project/domain/post/entity/Post.java
+++ src/main/java/com/sku/collaboration/project/domain/post/entity/Post.java
@@ -1,8 +1,11 @@
 package com.sku.collaboration.project.domain.post.entity;
 
+import com.sku.collaboration.project.domain.comment.entity.Comment;
 import com.sku.collaboration.project.domain.post.enums.PostType;
+import com.sku.collaboration.project.domain.post_like.entity.PostLike;
 import com.sku.collaboration.project.domain.user.entity.User;
 import com.sku.collaboration.project.global.common.BaseTimeEntity;
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.EnumType;
@@ -13,7 +16,10 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
+import java.util.ArrayList;
+import java.util.List;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -50,4 +56,10 @@ public class Post extends BaseTimeEntity {
   @Column(nullable = false)
   @Enumerated(EnumType.STRING)
   private PostType postType;
+
+  @OneToMany(mappedBy = "post", cascade = CascadeType.REMOVE, orphanRemoval = true)
+  private List<Comment> comments = new ArrayList<>();
+
+  @OneToMany(mappedBy = "post", cascade = CascadeType.REMOVE, orphanRemoval = true)
+  private List<PostLike> likes = new ArrayList<>();
 }
