da85c9ed824361c2bba5184558bd0d1ad30b1ed8
jaeyeon <kasto6329@gmail.com>
2025-07-07 15:03:26 +0900

    ✨ Feat: POST Like 생성

---
+++ src/main/java/com/sku/collaboration/project/domain/post_like/service/PostLikeService.java
@@ -0,0 +1,20 @@
+package com.sku.collaboration.project.domain.post_like.service;
+
+import java.util.List;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
+@Slf4j
+public class PostLikeService {
+
+  private final PostLikeRepository postLikeRepository;
+
+}
+
+
+
+
